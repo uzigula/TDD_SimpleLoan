90aa2b7dc99f63b6db354274aee884b611295c49
uzigula <uzigula@gmail.com>
2013-11-13 08:55:46 -0500

    Refactoring Test Step 2, using TestCase for all test

--- Financial.Test/QuotaEngineTest.cs
+++ Financial.Test/QuotaEngineTest.cs
@@ -17,29 +17,11 @@ namespace Financial.Test
         {
             loan = new QuotaEngine();
         }
-        [Test]
-        public void GetQuota_When_Capital_1000_and_Interes_1_Term_10_Return_101()
-        {
-            var quota = loan.GetQuota(1000, 0.01, 10);
-            quota.Should().Be(101);
-
-        }
-        [Test]
-        public void GetQuota_When_Capital_600_and_Interes_1_Term_10_Return_101()
-        {
-            var quota = loan.GetQuota(600, 0.01, 10);
-            quota.Should().Be(60.6);
-        }
-
-        [Test]
-        public void GetQuota_When_Capital_5000_and_Interes_10_Term_10_Return_101()
-        {
-            var quota = loan.GetQuota(5000, 0.1, 10);
-            quota.Should().Be(550);
-        }
 
         [Test]
         [TestCase(1000,1,10,101)]
+        [TestCase(600, 1, 10, 60.6)]
+        [TestCase(5000, 10, 10, 550)]
         public void GetQuota_Given_Capital_Rate_And_Term_Return_Quota(double capital, double rate, int term,
                                                                       double quotaExpected)
         {
