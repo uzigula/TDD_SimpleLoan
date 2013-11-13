56736f5f8ae0c5db1444eddb2cc36bad5306b76a
uzigula <uzigula@gmail.com>
2013-11-13 08:37:47 -0500

    First Test fail

---
+++ Financial.Test/QuotaEngineTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Financial;
+using FluentAssertions;
+
+namespace Financial.Test
+{
+    [TestFixture]
+    public class QuotaEngineTest
+    {
+        [Test]
+        public void GetQuota_When_Capital_1000_and_Interes_1_Term_10_Return_110()
+        {
+            var loan = new QuotaEngine();
+            var quota = loan.GetQuota(1000, 0.01, 10);
+            quota.Should().Be(110);
+
+        }
+    }
+}
---
+++ Financial/QuotaEngine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Financial
+{
+    public class QuotaEngine
+    {
+        public object GetQuota(int p1, double p2, int p3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
