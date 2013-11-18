96b85ecc8bfe04dfc3959edd8e127b1160c4ae69
uzigula <uzigula@gmail.com>
2013-11-18 07:29:11 -0500

    Loan Payment, First Test Failed

---
+++ Financial.Test/LoanPaymentTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Financial.Test
+{
+    public class LoanPaymentTest
+    {
+        [Test]
+        public void PerformPay_Given_Loan_Capital_5000_Rate_10_Term_10_When_Pay_550_Then_Have_first_Quota_cancelled()
+        {
+            var loan = new QuotaEngine(5000, 10 / 100, 10, DateTime.Now);
+            var schedule = loan.GetPaymentCalendar();
+            var loanPayment = new LoanPayment(schedule);
+            loanPayment.PerformPay(550);
+            schedule.Quotas[0].Cancelled.Should().Be(true);
+        }
+    }
+}
