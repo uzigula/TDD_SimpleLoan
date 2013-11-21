1b61e3e594e28ebc42462f440d937eb6704538cd
uzigula <uzigula@gmail.com>
2013-11-20 21:32:16 -0500

    Perform Payback, with default calculation, Test Failed

--- Financial.Test/LoanPaymentTest.cs
+++ Financial.Test/LoanPaymentTest.cs
@@ -33,6 +33,18 @@ namespace Financial.Test
             schedule.Quotas[0].RateAmountBalance.Should().Be(0, "Rate Balance it's wrong");
         }
 
-      
+        [Test]
+        public void PerformPay_Given_Loan_With_30_Day_Default_Capital_5000_Rate_10_Term_10_When_Pay_300_Then_CapitalRemain_is_253_And_RateAmountRemain_is_0_and_first_Quota_Not_Cancelled()
+        {
+            var loan = new QuotaEngine(5000, 0.1, 10, DateTime.Now.AddDays(-30));
+            var schedule = loan.GetPaymentCalendar();
+            var loanPayment = new LoanPayment(schedule);
+            loanPayment.PerformPay(300, DateTime.Now.AddDays(30));
+            schedule.Quotas[0].Cancelled.Should().Be(false);
+            schedule.Quotas[0].QuotaBalance.Should().Be(280, "Quota Balance it's wrong");
+            schedule.Quotas[0].CapitalBalance.Should().Be(280, "Capital Balance it's wrong");
+            schedule.Quotas[0].RateAmountBalance.Should().Be(0, "Rate Balance it's wrong");
+        }
+		
     }
 }
